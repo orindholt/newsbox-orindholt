a496e351bcd14d9c30e5279b5cc2ea12c8fbdf04
orindholt <83012141+orindholt@users.noreply.github.com>
2021-11-15 10:59:38 +0100

    Setup basic start files

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
+const babel = require("gulp-babel");
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+function buildStyles() {
+  return src('sass/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(purgecss({ content: ['*.html'] }))
+    .pipe(dest('dist'))
+}
+
+function buildScript() {
+  return src('js/**/*.js')
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('index.min.js'))
+    .pipe(dest('dist'))
+}
+
+function watchTask() {
+  watch(['sass/**/*.scss', 'js/**/*.js', '*.html'], series(buildStyles, buildScript));
+}
+
+exports.default = series(buildStyles, buildScript, watchTask);
